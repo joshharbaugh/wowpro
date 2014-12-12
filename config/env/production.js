19f877b0478685438da5ff0c7136688acfd6d4ef
Josh Harbaugh <pixelhavenllc@gmail.com>
2014-12-12 11:58:29 -0500

    Added aggregate option to production environments

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  aggregate: true,
   db: process.env.MONGOHQ_URL || 'mongodb://localhost/mean-prod',
   /**
    * Database options that will be passed directly to mongoose.connect
